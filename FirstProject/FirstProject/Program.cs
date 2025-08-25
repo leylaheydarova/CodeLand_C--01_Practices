c15735532b61f3cf2eacc23634682b5a267826b2
leylaheydarova <lbadalzade@gmail.com>
2025-08-25 21:03:27 +0400

    Methods

--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello");
+﻿int number = 1;
+short number2;
+number2 = (short)number;
+
+string eded = "2";
+var hasil = int.Parse(eded) * 2;
+
+string soz = "Cem: ";
+int cem = 2 + 4;
+soz = soz + cem.ToString();
+Console.WriteLine(soz);
+
+//eger kicik tipden boyuye kecid edirsense, birbasa cevrili
+//implicit casting (daxili cevirme) (mudaxilesiz, ozbasina)
+//boyuk tipden kiciye kecende, yaxud elaqesiz tipler cevrilende
+//ona bizim mudaxilemiz lazimdir. 
+//bu da olur Explicit casting (xarici cevirme - proqramist mudaxilesi)
